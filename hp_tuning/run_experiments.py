28af7fa895ea90d69d780fda29285939acb01acf
yarally <timyarally@hotmail.com>
2021-11-30 17:49:54 +0100

    Update config parser && implement random strategy

--- hp_tuning/run_experiments.py
+++ hp_tuning/run_experiments.py
@@ -1,10 +1,52 @@
 import json
-
-import DeBERTa.apps.run as deberta
+import os
 import sys
+import random
+
+ROOT_DIR = ''.join(f'{p}/' for p in os.path.dirname(os.path.abspath(__file__)).split('/')[:-1])[:-1]
+sys.path.append(ROOT_DIR)
+import DeBERTa.apps.run as DeBERTa
 
 INITIAL_SEED = 42
 MODEL_CONFIG_FILE = '/tmp/DeBERTa/tmp_model_config.json'
+MODEL_PARAMS = ["attention_probs_dropout_prob",
+                "hidden_act",
+                "hidden_dropout_prob",
+                "initializer_range",
+                "layer_norm_eps",
+                "max_relative_positions",
+                "padding_idx",
+                "pos_att_type",
+                "position_biased_input",
+                "relative_attention",
+                "type_vocab_size"]
+
+
+def run_custom_experiments(args):
+    print('Applying a custom strategy...')
+    with open(args.exp_config) as f:
+        config = json.load(f)
+    for i, experiment in enumerate(config['experiments']):
+        set_variables(args, {**experiment, **config['constants']}, i)
+        DeBERTa.run(args)
+
+
+def run_random_experiments(args):
+    print('Applying a random strategy...')
+    with open(args.exp_config) as f:
+        config = json.load(f)
+        for i in range(config['epochs']):
+            random.seed(INITIAL_SEED + i)
+            factors = {key: random.uniform(value[0], value[1]) for key, value in config['experiments'].items()} # TODO: Make compatible for non-numeric values!
+            set_variables(args, {**factors, **config['constants']}, i)
+            DeBERTa.run(args)
+
+
+def run_grid_experiments(args):
+    print('Applying a grid search strategy...')
+    pass
+
+
 # Unchangeable...
 # max_position_embeddings
 # vocab_size
@@ -12,8 +54,6 @@ MODEL_CONFIG_FILE = '/tmp/DeBERTa/tmp_model_config.json'
 # intermediate_size
 # num_attention_heads
 # num_hidden_layers
-
-
 def set_variables(args, experiment_config, index):
     """
     ### DeBERTa Variables
@@ -36,7 +76,7 @@ def set_variables(args, experiment_config, index):
     # lookahead_alpha
     # with_radam
     # opt_type
-    # learning_rate*
+    # learning_rate* --> Weird behavior for lr >= 3e-5
     # scale_steps
     # loss_scale*
     # dump_interval
@@ -51,15 +91,24 @@ def set_variables(args, experiment_config, index):
     # lr_schedule
     """
     with open(MODEL_CONFIG_FILE, 'w') as outfile:
-        json.dump(experiment_config['model_config'], outfile)
+        print(f'Current experiment configuration: {experiment_config}')
+        model_config = {param: value for param, value in experiment_config.items() if param in MODEL_PARAMS}
+        json.dump(model_config, outfile)
 
     setattr(args, 'seed', INITIAL_SEED + index)
-    for k, v in experiment_config['custom_config'].items():
-        setattr(args, k, v)
+    for key, value in experiment_config.items():
+        if key not in MODEL_PARAMS:
+            setattr(args, key, value)
 
 
-def set_constants(parser, task_name):
+def set_constants(parser):
     # Dirty Hack to work around required command line arguments
+    if '--exp_name' in sys.argv:
+        task_name = sys.argv[sys.argv.index('--exp_name') + 1]
+    else:
+        task_name = parser.get_default('exp_name')
+
+    print('Preparing for task:', task_name)
     sys.argv.extend(['--task_name', task_name, '--output_dir', f'/tmp/DeBERTa/exps/{task_name}'])
 
     parser.set_defaults(
@@ -73,17 +122,38 @@ def set_constants(parser, task_name):
     )
 
 
-def main(task_name, experiment_file):
-    parser = deberta.build_argument_parser()
-    set_constants(parser, task_name)
+def init_parser():
+    parser = DeBERTa.build_argument_parser()
+    parser.add_argument("--exp_strategy",
+                        default='random',
+                        type=str,
+                        required=False,
+                        choices=['random', 'grid', 'custom'],
+                        help="The strategy for hyper parameter optimization")
+    parser.add_argument("--exp_name",
+                        default='CoLA',
+                        type=str,
+                        required=False,
+                        choices=['CoLA', 'MNLI', 'MRPC', 'QNLI', 'QQP', 'RTE', 'SST-2', 'STS-B'],
+                        help="The type of task to run")
+    parser.add_argument("--exp_config",
+                        default='config.json',
+                        type=str,
+                        required=False,
+                        help="The config file of the experiments")
+    set_constants(parser)
     parser.parse_known_args()
-    args = parser.parse_args()
-    with open(experiment_file) as f:
-        experiments = json.load(f)
-    for i, experiment in enumerate(experiments):
-        set_variables(args, experiment, i)
-        deberta.run(args)
+    return parser.parse_args()
+
+
+def main(args):
+    strategies = {
+        "random": run_random_experiments,
+        "grid": run_grid_experiments,
+        "custom": run_custom_experiments
+    }
+    return strategies[args.exp_strategy](args)
 
 
 if __name__ == '__main__':
-    main('CoLA', 'test_experiment_config.json')
+    main(init_parser())
